f9f4944165f247c2d3bb421d5d9bc7e979b5c277
Shusnuke Oba <obashun22@yahoo.co.jp>
2023-09-06 19:27:31 +0900

    Update foodsページ作成 #22

--- frontend/src/components/organisms/FoodList.js
+++ frontend/src/components/organisms/FoodList.js
@@ -1,3 +1,16 @@
-export const FoodList = () => {
-  return <div>FoodList</div>;
+import { FoodItem } from "../molecules/FoodItem";
+
+export const FoodList = (props) => {
+  const { foods = [] } = props;
+  return (
+    <div>
+      {foods.map((food, index) => {
+        return (
+          <div className="p-1">
+            <FoodItem key={index} food={food} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
---
+++ frontend/src/constants/food.js
@@ -0,0 +1,49 @@
+export const CATEGORIES = {
+  vegetable: {
+    name: "野菜",
+    color: "#aaddaa",
+  },
+  milk: {
+    name: "牛乳",
+    color: "#aaddaa",
+  },
+  egg: {
+    name: "卵",
+    color: "#dddddd",
+  },
+  meat: {
+    name: "肉",
+    color: "#ffaaaa",
+  },
+  fish: {
+    name: "魚",
+    color: "#aaffaa",
+  },
+  fruit: {
+    name: "果物",
+    color: "#ffddaa",
+  },
+  other: {
+    name: "その他",
+    color: "#aaaaaa",
+  },
+};
+
+export const STORAGES = {
+  fridge: {
+    name: "冷蔵",
+    color: "#aaddaa",
+  },
+  freezer: {
+    name: "冷凍",
+    color: "#aaddaa",
+  },
+  room: {
+    name: "常温",
+    color: "#dddddd",
+  },
+  other: {
+    name: "その他",
+    color: "#aaaaaa",
+  },
+};
