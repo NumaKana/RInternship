74e944932fd6dd7d72e897d2f5f8b9ab14ed8742
Numakana <numakana6@gmail.com>
2023-09-06 11:08:25 +0900

    add haikei

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -4,8 +4,8 @@ import panda_amechan from "../../img/panda_amechan.gif"
 function Home() {
   return (
     <div className="App">
-        <div className="max-h-full max-w-full" style={{backgroundImage:`url(${haikei})`, width: "518px"}}></div>
-        <img src={panda_amechan} />
+        <img className="absolute z-10 top-1/4 left-auto" src={panda_amechan} alt="panda" />
+        <div className="h-screen w-screen relative" style={{backgroundImage:`url(${haikei})`, backgroundRepeat: 'no-repeat'}}></div>
     </div>
   );
 }
