7e018d7aa5f8959fa4f2900cb626d56322cc41ce
Shusnuke Oba <obashun22@yahoo.co.jp>
2023-09-06 11:51:45 +0900

    Update コンポーネント追加／class追加 #11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Foods from "./components/pages/Foods";
+import { Header } from "./components/organisms/Header";
+import { NavBar } from "./components/organisms/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Foods />
+      <NavBar />
     </div>
   );
 }
---
+++ frontend/src/components/pages/Foods.js
@@ -0,0 +1,11 @@
+import Radio from "@mui/joy/Radio";
+
+const Foods = () => {
+  return (
+    <div>
+      FoodsPage
+      <Radio />
+    </div>
+  );
+};
+export default Foods;
