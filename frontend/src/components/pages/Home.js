c0e64b78fa3d0fd377ed4c93ef312ecb44b9d196
Numakana <numakana6@gmail.com>
2023-09-06 16:18:15 +0900

    Modified パンダの位置 #4

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -9,6 +9,16 @@ import sasa_golden from "../../img/sasa_golden.png"
 import next_arrow from "../../img/icon/next.png"
 import prev_arrow from "../../img/icon/prev.png"
 
+
+
+function feed_sasa(){
+  console.log("笹をあげました");
+}
+
+function feed_premiumsasa(){
+  console.log("いい笹をあげました");
+}
+
 function NextArrow(props) {
   const { className, style, onClick } = props;
   return (
@@ -46,29 +56,31 @@ function Home() {
   return (
     <div className="App">
       <div className="h-screen w-screen relative" style={{backgroundImage:`url(${haikei})`, backgroundRepeat: 'no-repeat'}}>
+        <img className="absolute z-10 top-1/3 left-auto" src={panda_amechan} alt="panda" />
+
+        <Slider className="top-3/4 left-1/8" {...settings}>
+          <div>
+            <button className="inset-y-2/3 inset-x-1/4 h-12 w-48 overflow-hidden rounded-2xl bg-base text-lg font-bold text-main"
+              onClick={feed_sasa}>
+              <div className="flex w-full">
+                <img className="w-11" src={sasa_golden} alt="sasa" />
+                <p className="relative top-2 w-full">いい笹をあげる</p>
+              </div>
+            </button>
+          </div>
+
+          <div>
+            <button className="inset-y-2/3 inset-x-1/4 h-12 w-48 overflow-hidden rounded-2xl bg-base text-lg font-bold text-main"
+              onClick={feed_premiumsasa}>
+              <div className="flex w-full">
+                <img className="w-11" src={sasa} alt="sasa" />
+                <p className="relative top-2 w-full">笹をあげる</p>
+              </div>
+            </button>
+          </div>
+        </Slider>
+
         <NavBar />
-        <img className="absolute z-10 top-1/4 left-auto" src={panda_amechan} alt="panda" />
-
-          <Slider className="top-3/4 left-1/8" {...settings}>
-            <div>
-              <button className="inset-y-2/3 inset-x-1/4 h-12 w-48 overflow-hidden rounded-2xl bg-base text-lg font-bold text-main">
-                <div className="flex w-full">
-                  <img className="w-11" src={sasa_golden} alt="sasa" />
-                  <p className="relative top-2 w-full">いい笹をあげる</p>
-                </div>
-              </button>
-            </div>
-
-            <div>
-              <button className="inset-y-2/3 inset-x-1/4 h-12 w-48 overflow-hidden rounded-2xl bg-base text-lg font-bold text-main">
-                <div className="flex w-full">
-                  <img className="w-11" src={sasa} alt="sasa" />
-                  <p className="relative top-2 w-full">笹をあげる</p>
-                </div>
-              </button>
-            </div>
-          </Slider>
-
       </div>
     </div>
   );
